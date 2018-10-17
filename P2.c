b6cbd3900af2f7193355316390637454b23c5084
marcusvims <42359644+marcusvims@users.noreply.github.com>
2018-10-16 23:06:06 -0300

    Update P2.c

--- P2.c
+++ P2.c
@@ -1,118 +1,119 @@
 /* P2.c */
-/* Marcus Vinícius Meireles Silva*/
+/* Marcus VinÃ­cius Meireles Silva*/
 /* 11811EBI006*/
-#include<stdio.h>
-void trocar (char vet[])
-{
-	int i, h=0, n= 0, t= 0;
-	for (i=0;vet[i]!='\0';i++)
-		n=n+1;
-	t=n;
-	n=n-1;
-	for (i=0;i<n; i++)
-	{
-		h= vet[i];
-		vet[i] = vet[n];
-		vet[n] = h;
-		n=n-1;
-	}
-	vet[t]='\0';
+
+#include <stdio.h>
+
+int pow2(int i){
+	int resp=1, j;
+	for(j=0; j<i; j++)
+		resp= resp*2;
+	return resp;
 }
-void binario(int vet)
+
+void inverter (char vet[])
 {
-	int i,z,numero[256],n=0,r;
-	r=vet;
-	for(i=0;n<1;i++)
+	int aux = 0, i, cont = 0, total = 0;
+	for (i=0; vet[i] != '\0'; i++)
+		cont++;
+	total = cont;
+	cont--;
+	for (i=0; i<cont; i++)
 	{
-		numero[i] = r%2;
-		r=r/2;
-		if(r==1 || r==0)
+		aux = vet[i];
+		vet[i] = vet[cont];
+		vet[cont] = aux;
+		cont--;
+	}
+	vet[total] = '\0';
+}
+
+int bindec(char bits[]){
+	inverter(bits);
+	int i, soma, j;
+	for(i=0; bits[i]!= '\0' ; i++){
+		if(bits[i]=='1')
+			soma = soma+bits[i]*pow2(i);
+	}
+	return soma/49+1;
+}
+
+void decbin (int num)
+{
+	int vet_bin[256], i, j, resto, cont=0;
+	resto = num;
+	for (i=0; cont<1 ; i++)
+	{
+		vet_bin[i] = resto % 2;
+		resto = resto/2;
+		if (resto == 1 || resto == 0)
 		{
-			numero[i+1]=r;
-			z=i+1;
-			n++;
+			vet_bin[i+1] = resto;
+			j=i+1;
+			cont++;
 		}
 	}
-	printf("O numero na base binaria vale: ");
-	for(i=z;i>=0;i--)
+	printf ("Resultado: ");
+	for (i=j; i>=0; i--)
+		printf ("%d", vet_bin[i]);
+}
+
+int main(){
+	int e, numero;
+	char bits[256];
+	printf("1. Binario para Decimal\n2. Binario para Hexadecimal\n3. Hexadecimal para Decimal\n4. Hexadecimal para Binario\n5. Decimal para Binario\n6. Decimal para Hexadecimal\n7. Octal para Decimal\n8. Decimal para Octal\n\nEscolha: ");
+	scanf("%i", &e);
+	getchar();
+	switch(e)
 	{
-		printf(" %i", numero[i]);
+		case 1:
+			printf("Digite o numero: ");
+			scanf("%s", bits);
+			getchar();
+			printf("\nResultado: %i", bindec(bits));
+			break;
+		case 2:
+			printf ("Digite o numero: ");
+			scanf ("%s", bits);
+			getchar();
+			printf ("\nResultado: %x", bindec(bits));
+			break;
+		case 3:
+			printf ("Digite o numero: ");
+			scanf ("%x", &numero);
+			getchar();
+			printf ("\nResultado: %d", numero);
+			break;
+		case 4:
+			printf ("Digite o numero: ");
+			scanf ("%x", &numero);
+			getchar();
+			decbin(numero);
+			break;
+		case 5:
+			printf ("Digite o numero: ");
+			scanf("%d", &numero);
+			getchar();
+			decbin(numero);
+			break;
+		case 6:
+			printf ("Digite o numero: ");
+			scanf("%d", &numero);
+			getchar();
+			printf ("\nResultado: %x", numero);
+			break;
+		case 7:
+			printf ("Digite o numerol: ");
+			scanf("%o", &numero);
+			getchar();
+			printf("\nResultado: %d", numero);
+			break;
+		case 8:
+			printf ("Digite o numero: ");
+			scanf("%d", &numero);
+			getchar();
+			printf ("\nResultado: %o", numero);
+			break;
 	}
-}
-int potencia (int resul1, int resul2)
-{
-	int i, r=1;
-	for (i=0; i<resul2; i++)
-		r=r*resul1;
-	return r;
-}
-int decimal (char vet[])
-{
-	trocar(vet);
-	int somatorio=0, i;
-	for (i=0;vet[i]!='\0';i++)
-		if (vet[i]=='1')
-			somatorio= somatorio + vet[i]*potencia(2, i);
-	return somatorio/49;
-}
-int main()
-{
-	char vet[256];
-	int x;
-	printf("\nEscolha a conversao que deseja fazer:\n1. binario para decimal\n2. binario para hexadecimal \n3. hexadecimal para decimal \n4. hexadecimal para binario\n5.decimal para binario\n6. decimal para hexadecimal\n7. octal para decimal\n8. decimal para octal");
-			if(x==1)
-			{
-				pritnf("\nDigite um numero binario para convertar em decimal: ");
-				scanf("%s", &vet);
-				printf("\nO valor desse numero na base decimal vale: %i",decimal(vet));
-			}
-			if(x==2)
-			{
-				print("\nDigite um numero binario para hexadecimal: ");
-				scanf("%s", &vet);
-				printf("\nO valor desse numero na base hexadecimal vale: %x",decimal(vet));
-			}
-			if(x==3)
-			{
-				int (vet);
-				printf("\nDigite um numero hexadecimal para decimal: ");
-				scanf("%d", &vet);
-				printf("\nO valor desse numero na base decimal vale: %d", vet);
-			}
-			if(x==4)
-			{
-				int (vet);
-				printf("\nDigite um numerohexadecimal para binario: ");
-				scanf("%d", &vet);
-				binario(x);
-			}
-			if(x==5)
-			{
-				int (vet);
-				printf("\nDigite um decimal para binario: ");
-				scanf("%d", &vet);
-				binario(x);
-			}
-			if(x==6)
-			{
-				int (vet);
-				printf("\nDigite um decimal para hexadecimal: ");
-				scanf("%d", &vet);
-				printf("\nO valor desse numero na base hexadecimal vale: %x", vet);
-			}
-			if(x==7)
-			{
-				int (vet);
-				printf("Digite um octal para decimal: ");
-				scanf("%d", &vet);
-				printf("\nO valor desse numero na base decimal vale: %d", vet);
-			}
-			if(x==8)
-			{
-				int (vet);
-				printf("Digite um decimal para octal: ");
-				scanf("%d", &vet);
-				printf("\nO valor desse numero na base octal vale: %o", vet);
-			}
 	return 0;
 }
